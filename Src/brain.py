b12050542d85d3a428e80b364ea2450f8441be4f
Solonioka <solonioka@gmail.com>
2020-10-13 16:45:29 -0400

    TEST MAX

--- Src/brain.py
+++ Src/brain.py
@@ -4,7 +4,7 @@ from Src.Models.turnInformation import TurnInformation
 
 
 class Brain(metaclass=Singleton):
-
+    cmpt = 0
 
     def on_next_move(turn_info: TurnInformation):
         '''
@@ -12,50 +12,42 @@ class Brain(metaclass=Singleton):
         @param turn_info: Information from the current turn
         @return: The direction your AI chose as his next move.
         '''
-        cpt = 0
+
         print(
             "the game server wants to know your next move and you have the following informations : the id is {0} and the current map is {1} ".format(
                 turn_info.SelfId, turn_info.Map))
-        global cpt
-        if cpt % 4 == 0:
+        global cmpt
+        if cmpt % 4 == 0:
             return Direction._UP
-        elif cpt % 4 == 1:
+        elif cmpt % 4 == 1:
             return Direction._LEFT
-        elif cpt % 4 == 2:
+        elif cmpt % 4 == 2:
             return Direction._DOWN
         else:
             return Direction._RIGHT
 
-
-
         # global choix prochain move
         # Fonctions utiles:
-            # Calcul du nombre de bloc nous séparant de notre zone
-            # retourne blocs, choix prochain move
-
+        # Calcul du nombre de bloc nous séparant de notre zone
+        # retourne blocs, choix prochain move
 
         # Si nb_pas_restants == distance notre_tête-zone:
-            #Retour en ligne droite vers notre zone
-
-
+        # Retour en ligne droite vers notre zone
 
         # Si une des possibilités de prochain move == W:
-            # trouver options où pas de mur next move
-            # parmis ces options, trouver celle plus proche de notre zone:
-                # si 2 options égales: Choisir option la plus loin de ennemi le plus proche
+        # trouver options où pas de mur next move
+        # parmis ces options, trouver celle plus proche de notre zone:
+        # si 2 options égales: Choisir option la plus loin de ennemi le plus proche
 
         # Défensive:
-            # Si distance notre zone + 3 >= distance tête adversaire / - notre coup:
-                #
-
+        # Si distance notre zone + 3 >= distance tête adversaire / - notre coup:
+        #
 
         # Offensive:
-            # Si tête adversaire dans notre zone AND notre tête dans notre zone:
-                # Direction vers cou/tête adversaire
-
+        # Si tête adversaire dans notre zone AND notre tête dans notre zone:
+        # Direction vers cou/tête adversaire
 
         # As a default we put that the direction to UP.
-
 
     def on_finalized(turn_info: TurnInformation):
         '''
@@ -63,4 +55,5 @@ class Brain(metaclass=Singleton):
         It could be used to train the AI for example.
         @param turn_info: Information from the current turn
         '''
-        print("the game has ended and the received id from the game server is {0} with the following map {1}".format(turn_info.SelfId, turn_info.Map)) 
+        print("the game has ended and the received id from the game server is {0} with the following map {1}".format(
+            turn_info.SelfId, turn_info.Map))
