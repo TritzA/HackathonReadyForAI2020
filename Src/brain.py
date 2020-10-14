afe425c0523b429f66222b496357fd3e042dfe54
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-10-14 15:20:39 -0400

    Update brain.py

--- Src/brain.py
+++ Src/brain.py
@@ -19,7 +19,6 @@ def randomize(a: bool, b: bool, c: bool, d: bool):
 
 
 def toucheAuVide(x, y, matrice):
-    print('OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
     if matrice[x][y - 1] == '':
         return True
     if matrice[x][y + 1] == '':
@@ -68,7 +67,7 @@ class Brain(metaclass=Singleton):
 
         if array[x][y] == fg or array[x][y] == fj:  # si notre position actuelle est sur notre corps
             # aller dans le vide
-            print('hhhhhhhhhhhhhhhhhh')
+            
             if left == '':
                 a = True
             if up == '':
@@ -79,7 +78,7 @@ class Brain(metaclass=Singleton):
                 d = True
             if a or b or c or d:
                 return randomize(a, b, c, d)
-            print('fsdfgsdfgsdfgsdfgsdfg')
+            
             # aller sur le cou de l'adversaire
             if left[0] == 'p':
                 a = True
@@ -103,7 +102,7 @@ class Brain(metaclass=Singleton):
                 d = True
             if a or b or c or d:
                 return randomize(a, b, c, d)
-            print('zdfgsdgdfgsdfgsdfgsdfg')
+            
             if left == f and toucheAuVide(x, y - 1, array):
                 a = True
             if up == f and toucheAuVide(x - 1, y, array):
@@ -138,19 +137,6 @@ class Brain(metaclass=Singleton):
         if down == f:
             return Direction._DOWN
 
-        # cmpt=random.randint(0,4)
-        # if cmpt % 4 == 0:
-        #    cmpt += 1
-        #    return Direction._UP
-        # elif cmpt % 4 == 1:
-        #    cmpt += 1
-        #    return Direction._LEFT
-        # elif cmpt % 4 == 2:
-        #    cmpt += 1
-        #    return Direction._DOWN
-        # else:
-        #    cmpt += 1
-        #    return Direction._RIGHT
 
     def on_finalized(turn_info: TurnInformation):
         '''
